3434517519044033a511b5e7d9bef4759f221912
zenix-s <sergiofs19@gmail.com>
2024-01-25 09:32:38 +0100

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://zenix-s.github.io',
-  base: '/my-repo',
+  // base: '/my-repo',
 });
